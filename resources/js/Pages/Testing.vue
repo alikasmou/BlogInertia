9dba39c195254b103452cfb9bd4bbfe75fc25e05
ALI KASMOU <alikasmou@ALIs-MacBook-Pro.local>
2021-05-15 21:42:50 +0300

    Installation, Jetstram Inertia, User database seeder

---
+++ resources/js/Pages/Testing.vue
@@ -0,0 +1,17 @@
+<template>
+    <app-layout>
+sdfvdfvwe
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    //import Welcome from '@/Jetstream/Welcome'
+
+    export default {
+        components: {
+            AppLayout
+            //Welcome,
+        },
+    }
+</script>
